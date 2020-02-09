60c08a6b6bc3bcda02159e9680952560cc9791b0
dmitrijsg123 <59980795+dmitrijsg123@users.noreply.github.com>
2020-02-09 17:25:20 +0000

    Delete Week3_homework_reversed_step2.py

--- Week3_homework_reversed_step2.py
+++
@@ -1,8 +0,0 @@
-# A program that asks a user to input a string and outputs every second letter in reverse order
-
-# The quick brown fox jumps over the lazy dog.
-
-x = "The quick brown fox jumps over the lazy dog."
-
-x_reversed = (x[::-2])
-print(x_reversed)
