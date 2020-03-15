4bc764335ee2a9773eff7e21f0ae64a447e7b70d
dmitrijsg123 <dmitrijsg123@gmail.com>
2020-03-15 20:14:22 +0000

    First commit Week_8

---
+++ Week_8_Homework.py
@@ -0,0 +1,25 @@
+# Write a program that displays a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] on the one set of axes.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(1.0,4.0,0.0001)
+
+f = x
+g = x**2
+h = x**3
+
+plt.plot(x,f,"g.")
+plt.plot(x,g,"b.")
+plt.plot(x,h,"r.")
+plt.title("f, g and h lines")
+plt.xlabel("x")
+plt.ylabel("y")
+
+plt.show()
+
+
+
+
+
+
