50afcdeadac8f548a8465a676a483f9f89128033
dmitrijsg123 <59980795+dmitrijsg123@users.noreply.github.com>
2020-04-02 23:10:10 +0100

    latest commit - adding input function

--- Week3_homework_reversed_step2.py
+++ Week3_homework_reversed_step2.py
@@ -1,8 +1,9 @@
 # A program that asks a user to input a string and outputs every second letter in reverse order
 
-# The quick brown fox jumps over the lazy dog.
-
-x = "The quick brown fox jumps over the lazy dog."
+x = str(input("Enter any phrase now: "))  
 
 x_reversed = (x[::-2])
 print(x_reversed)
+
+# For example, the user enters: "I've seen it all now, a Rabbitte chasing a Fox around Croke Park!"
+# The output then is: "!rPeoCdur o  nsh tibRa,o l ine vI"
