090c1a7e59befa1e8e366d63e982b3b6080bfc96
dmitrijsg123 <59980795+dmitrijsg123@users.noreply.github.com>
2020-04-02 23:53:21 +0100

    latest commit - few corrections and simplifications

--- Weekday(Week_5_homework).py
+++ Weekday(Week_5_homework).py
@@ -4,23 +4,23 @@
 import datetime
 
 x = datetime.datetime.now()
-
-y = (input)('Ask me a tricky question, like "Weekday or weekend?"')
+y = x.weekday()
+z = str(input('Ask me a tricky question, like "Weekday or weekend?"'))
 
 question = ("Weekday or weekend?")
 
-while y == question:
+while z == question:
 
-    if x.weekday() <= 3:                                                    # if today is Monday to Thursday - program answers
+    if y <= 3:                                                          # if today is Monday to Thursday - program answers
         print("Unfortunately today is still a weekday.")                   
         break                   
-    elif x.weekday() == 4:                                                  # If today is Friday - program answers
+    elif y == 4:                                                        # If today is Friday - program answers
         print("Hold on, still weekday but nearly there, my friend")        
         break
 
-    elif x.weekday() == 5 or 6:                                             # If today is Saturday or Sunday - program answers
+    elif y == 5 or 6:                                                   # If today is Saturday or Sunday - program answers
         print("It is weekend, thanks God!")                     
         break                     
     
-else:                                                                       # If incorrect input - program answers
+else:                                                                   # If incorrect input - program answers
     print("I don't understand your question")                               
