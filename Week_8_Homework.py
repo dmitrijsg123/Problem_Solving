cd0c3afc28ecef2ccbc4cdf5b0a5e12831f17e18
dmitrijsg123 <59980795+dmitrijsg123@users.noreply.github.com>
2020-04-03 00:17:15 +0100

    latest commit - minor additions

--- Week_8_Homework.py
+++ Week_8_Homework.py
@@ -9,9 +9,9 @@ f = x
 g = x**2
 h = x**3
 
-plt.plot(x,f,"g.")
-plt.plot(x,g,"b.")
-plt.plot(x,h,"r.")
+plt.plot(x,f,"g.")                # f line in green
+plt.plot(x,g,"b.")                # g line in blue
+plt.plot(x,h,"r.")                # h line in red
 plt.title("f, g and h lines")
 plt.xlabel("x")
 plt.ylabel("y")
