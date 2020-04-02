a333d9e4bdeda16bc8a487398959984991732bd7
dmitrijsg123 <59980795+dmitrijsg123@users.noreply.github.com>
2020-04-02 22:28:49 +0100

    latest commit
    changed the single quote line 6 into double quote to make same as on line 7, changed typo line 14

--- bmi.py
+++ bmi.py
@@ -3,7 +3,7 @@
 print("Dimitri BMI")
 
 weight = float(input("Enter weight in kilograms: "))
-height = float(input('Enter height in metres: '))
+height = float(input("Enter height in metres: "))
 
 BMI = weight/(height **2)
 print ("Dimitri, your super BMI is", BMI)
@@ -11,4 +11,4 @@ print ("Dimitri, your super BMI is", BMI)
 # Run the program first time - enter weight in kilograms - 70
 # Run the program second time - enter height in metres - 1.76
 # Run the program again to get the BMI result and start thinking about joining the gym 
-# If you want the end result as integer, line 8 should read 'BMI = (int(weight/height * 2))'
+# If you want the end result as integer, line 8 should read 'BMI = (int(weight/height ** 2))'
